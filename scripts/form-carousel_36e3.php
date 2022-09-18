fd4f4b0f2e722ffe23ae85a217631efa4373252a
Matvey <83799026+Mioop75@users.noreply.github.com>
2022-09-18 14:41:17 +0300

    Add files via upload

---
+++ scripts/form-carousel_36e3.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'Отправка новой формы',
+    'email_message' => 'У вас есть новая отправка формы',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => '',
+    'to' => 'mariaasia@bk.ru'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => false,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'phone' => array(
+    'order' => 3,
+    'type' => 'tel',
+    'label' => 'phone',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'phone\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 4,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
